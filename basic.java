9c70f78207d1a8fec45ab7119dcbaf17732f8092
VipulGajbhiye09 <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-01 23:45:33 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -1,9 +1,21 @@
 package javaapp;
+import java.util.Scanner;
 
 public class JavaApp {
 
     public static void main(String[] args) {
-        
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter number of values required :");
+        int a=s.nextInt();
+        System.out.println("Enter the values :");
+        int sum=0;
+        for(int i=1;i<=a;i++){
+            int n = s.nextInt();
+            sum = sum +n;
+        }
+        System.out.println("Addition of values is :"+sum);
+        int avg = sum/a;
+        System.out.println("Average of values is :"+avg);
     }
     
 }
