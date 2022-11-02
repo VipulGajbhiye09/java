b64aa3d4425234cdb516d0234dba942d7598e9b1
VipulGajbhiye09 <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-02 21:59:44 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 public class JavaApplication1 
 {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Scanner s = new Scanner (System. in);
         System.out.println("Enter the value ");
         int a = s.nextInt();
         int fact = 1;
-        for (int i= 1;i<=a;i++) 
-        {
+        for (int i= 1;i<=a;i++) {
             fact = i*fact;
         }
         
