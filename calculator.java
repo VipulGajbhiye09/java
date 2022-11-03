1d9fda3a6935d54415db2a30ff47444f2c363c05
VipulGajbhiye09 <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-03 23:36:34 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,1 +1,15 @@
+package calculator;
+import java.util.Scanner;
 
+public class Calculator {
+
+    public static void main(String[] args) {
+        int a,b,c; 
+        String operator;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the operator you want to use");
+        operator = s.nextLine(); 
+        System.out.println("Enter first value"); 
+        a = s.nextInt(); 
+        System.out.println("Enter second value"); 
+        b = s.nextInt();
