f359391189983bbede8bcc03fd920a6780431d88
VipulGajbhiye09 <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-04 22:52:47 +0530

    Update rectangle.java

--- rectangle.java
+++ rectangle.java
@@ -1,1 +1,15 @@
+package rectangle;
+import java.util.Scanner;
 
+class rect{
+    int l,b;
+    String colour;
+    void get_length(){
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter length");
+        l = s1.nextInt();
+    }
+    void get_width(){
+        Scanner s2 = new Scanner(System.in);
+        System.out.println("Enter width");
+        b = s2.nextInt();
