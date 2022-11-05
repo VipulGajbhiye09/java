8ed4858ffb2e25f7c88d7d93c098086eab4aecc4
VipulGajbhiye09 <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-05 23:33:20 +0530

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -4,3 +4,9 @@ class Sample{
     Sample(){
         System.out.println("This is default constructor");
     }
+    
+    Sample(int x,int y){
+        System.out.println("This is parameterized constructor ");
+        System.out.println("X :"+x+"\nY :"+y);
+    }
+}
