0557d7ae599b004dbc217182437d22f365f7002f
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-07 22:49:06 +0530

    Update matrixadd.java

--- matrixadd.java
+++ matrixadd.java
@@ -1,1 +1,24 @@
+package matrixadd;
 
+import java.util.*;
+
+public class MatrixAdd {
+
+    public static void main(String[] args) {
+        int arr1[][]=new int[2][2];
+        int arr2[][]=new int[2][2];
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter First Matrix : ");
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                arr1[i][j]=s.nextInt();
+            }
+            System.out.println();
+        }
+        System.out.println("Enter Second Matrix : ");
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                arr2[i][j]=s.nextInt();
+            }
+            System.out.println();
+        }
