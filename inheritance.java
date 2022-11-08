e9921ff62db265da19ea8d8a9914a9341e5c75fd
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-08 23:18:04 +0530

    Update inheritance.java

--- inheritance.java
+++ inheritance.java
@@ -31,3 +31,22 @@ class Football_player extends Player{
     }
 }
 
+public class Inheritance {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Cricket_player obj1 = new Cricket_player();
+        Hockey_player obj2 = new Hockey_player();
+        Football_player obj3 = new Football_player();
+        obj1.name="Sachin";
+        obj1.score=162;
+        obj2.name="Gaurav";
+        obj2.score=29;
+        obj3.name="Arush";
+        obj3.score=82;
+        obj1.display();
+        obj2.display();
+        obj3.display();   
+    }
+    
+}
