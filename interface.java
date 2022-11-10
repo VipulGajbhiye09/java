bd70a9a450cf628d0918d73dea85f6d837733fae
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-10 23:42:40 +0530

    Update interface.java

--- interface.java
+++ interface.java
@@ -21,3 +21,14 @@ class B1 implements B{
     }
 }
 
+public class Interface {
+
+    public static void main(String[] args) {
+        A1 o1 =new A1();
+        B1 o2 =new B1();
+        o1.disp1();
+        o2.disp2();
+    }
+    
+}
+
