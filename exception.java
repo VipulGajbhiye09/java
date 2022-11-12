aa2ef70fb608a044652e554edc250a8eb45ee755
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-12 22:44:48 +0530

    Update exception.java

--- exception.java
+++ exception.java
@@ -1,1 +1,8 @@
-
+class Main
+{
+    public static void main(String args[])
+    {
+        try {
+            int a = 30, b = 0;
+            int c = a/b;  // cannot divide by zero
+            System.out.println ("Result = " + c)
