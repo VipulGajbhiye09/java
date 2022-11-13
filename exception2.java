13d3aa847c5ca4144e3f7ebd5ec4a1dc9529ee4e
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-13 23:19:06 +0530

    Update exception2.java

--- exception2.java
+++ exception2.java
@@ -5,3 +5,9 @@ class Main
         try{
             int a[] = new int[5];
             a[6] = 289;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println ("Array Index is Out Of Bounds");
+        }
+    }
+}
