7163fb8c6e5a52e8eba319a7a9e4f9aabe90d6b2
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-14 22:29:54 +0530

    Update exception3.java

--- exception3.java
+++ exception3.java
@@ -6,3 +6,9 @@ class StringIndexOutOfBound_Demo
             String a = "This is like a code "; 
             char c = a.charAt(28); // accessing 29th element
             System.out.println(c);
+        }
+        catch(StringIndexOutOfBoundsException e) {
+            System.out.println("StringIndexOutOfBoundsException");
+        }
+    }
+}
