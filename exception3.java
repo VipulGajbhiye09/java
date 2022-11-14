01d146950ef236f76078163854dc9ae771194d57
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-14 22:29:40 +0530

    Update exception3.java

--- exception3.java
+++ exception3.java
@@ -1,1 +1,8 @@
-
+class StringIndexOutOfBound_Demo
+{
+    public static void main(String args[])
+    {
+        try {
+            String a = "This is like a code "; 
+            char c = a.charAt(28); // accessing 29th element
+            System.out.println(c);
