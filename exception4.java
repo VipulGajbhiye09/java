d6b479c0b0ea291271805c126717f51a3dfee97f
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-15 23:03:17 +0530

    Update exception4.java

--- exception4.java
+++ exception4.java
@@ -1,3 +1,6 @@
 public class ClassNotFoundException_Demo
 {
     public static void main(String[] args) {
+        try{
+            Class.forName("Class1");   // Class1 is not defined
+        }        
