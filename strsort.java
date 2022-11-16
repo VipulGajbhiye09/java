82e1c829d30955b7d9e1c81a166aad8afe5472ed
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-16 23:18:12 +0530

    Update and rename strsort to strsort.java

---
+++ strsort.java
@@ -0,0 +1,12 @@
+package strsort;
+import java.util.*;
+
+public class Strsort {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Original Array is :");
+        String str = s.nextLine();
+        char arr[]  = str.toCharArray();
+        char temp;
+        int i=0;
