c6322fd58562e6642abcbae8a8445ad25c3bc0a3
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-16 23:18:32 +0530

    Update strsort.java

--- strsort.java
+++ strsort.java
@@ -10,3 +10,15 @@ public class Strsort {
         char arr[]  = str.toCharArray();
         char temp;
         int i=0;
+        while(i<arr.length){
+            int j=i=i;
+            while(j<arr.length){
+                if(arr[i]>arr[j]){
+                   temp=arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=temp;
+                }
+                j+=1;
+            }
+            i+=1;
+        }
