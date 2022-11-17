f635903365cd01a4c6d95817dcd850bf8cb881a8
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-17 23:26:19 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -8,3 +8,11 @@ public class JavaApp {
         System.out.println("Enter upper bound to find prime number ");
         int n = s.nextInt();
         
+        for(int i=1;i<=n;i++){
+          int count =0;  
+          for(int j=1;j<=i;j++){
+            if(i%j==0){
+                count++;
+            }
+            
+        }        
