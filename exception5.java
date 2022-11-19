13916c6f2d59a73bdad150f95370ace454516a23
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-19 23:17:31 +0530

    Update exception5.java

--- exception5.java
+++ exception5.java
@@ -4,3 +4,7 @@ class NullPointer_Demo
 {
     public static void main(String args[])
     {
+         try {
+            String a = null; //null value
+            System.out.println(a.charAt(0));
+        }
