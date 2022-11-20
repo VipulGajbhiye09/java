ce933c68db20cabdacd0ad257078e2f931856f36
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-20 23:12:21 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -1,1 +1,16 @@
-
+package bubblesort;
+import java.util.*;
+ 
+class Sort{
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swapping arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
