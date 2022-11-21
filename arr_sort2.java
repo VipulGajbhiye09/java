a3efcebc26b277cff62d6e3caa3273c1b17fefd0
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-21 23:19:51 +0530

    Update arr_sort2.java

--- arr_sort2.java
+++ arr_sort2.java
@@ -2,4 +2,10 @@ import java.util.*;
  
 class Main {
     public static void main(String args[])
+    {
+        int[] arr = { 5, -2, 23, 7, 87, -42, 509 };
+        System.out.println("The original array is: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
   
