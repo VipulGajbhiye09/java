9f85fb6ea487f9c2d89337bd36746a6fa2f2813d
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-23 23:09:14 +0530

    Update exception6.java

--- exception6.java
+++ exception6.java
@@ -1,1 +1,8 @@
-
+import java.util.*; 
+import java.io.*;
+ 
+class Main {
+      public static void  print(int a,int b)
+     {
+         System.out.println("Addition of Positive Integers :"+(a+b));
+     }
