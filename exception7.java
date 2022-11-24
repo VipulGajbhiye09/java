2c4893df95bf6d1c128b6add1afbb6ec6229875c
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-24 21:00:42 +0530

    Update exception7.java

--- exception7.java
+++ exception7.java
@@ -4,5 +4,11 @@ class Main
     public static void main (String args[])
     {
         try {
-            String a = null; //null value
+            String a = null;   //null value
             System.out.println(a.charAt(0));
+        } 
+        catch(NullPointerException e) {
+            System.out.println(" NullPointerException  ");
+        }
+    }
+}
