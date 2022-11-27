53ad5b295febe52163da6fad31cae76ee6cddd4d
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-27 10:19:43 +0530

    Update loop3.java

--- loop3.java
+++ loop3.java
@@ -4,3 +4,8 @@ public class Main {
  
     public static void main(String args[])
     {
+        for (int i = 0; i <= 15; i++) {
+ 
+            if (i % 5==0) {
+                continue;
+            }
