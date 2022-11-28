fe96de45ced6955e1a6176d3e406dd7ef14f8872
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-28 19:01:54 +0530

    Update loop4.java

--- loop4.java
+++ loop4.java
@@ -6,3 +6,9 @@ public class Main {
         for (int i = 0; i < 50; i++) {
             if (i == 15)
                 break;
+ 
+            System.out.println("i: " + i);
+        }
+        System.out.println("Loop complete.");
+    }
+}
