93fe977f38fe3e0cb8ae90c9852356e3232a98a0
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-11-29 16:46:39 +0530

    Update loop5.java

--- loop5.java
+++ loop5.java
@@ -6,4 +6,33 @@ public class Main {
     {
         int day = s.nextInt();
         String dayString;
+        
+        switch (day) {
+                
+        case 1:
+            dayString = "Monday";
+            break;
+                
+        case 2:
+            dayString = "Tuesday";
+            break;
+
+        case 3:
+            dayString = "Wednesday";
+            break;
+
+        case 4:
+            dayString = "Thursday";
+            break;
+
+        case 5:
+            dayString = "Friday";
+            break;
   
+        case 6:
+            dayString = "Saturday";
+            break;
+
+        case 7:
+            dayString = "Sunday";
+            break;  
