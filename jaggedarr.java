36870658321b8fdb4f56abf8ffa219ca3bf62232
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-01 18:34:34 +0530

    Update jaggedarr.java

--- jaggedarr.java
+++ jaggedarr.java
@@ -1,1 +1,6 @@
+// Program to demonstrate 2-D jagged array in Java
 
+public class Main {
+    public static void main(String[] args)
+    {     
+        int arr[][] = new int[2][]; // Declaring 2-D array with 2 rows
