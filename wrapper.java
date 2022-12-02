2d0cbc2d45edd3278282a6e88ae7d2612e1241d5
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-02 22:15:02 +0530

    Update wrapper.java

--- wrapper.java
+++ wrapper.java
@@ -3,3 +3,10 @@ import java.util.*;
 
 public class Main
 {
+    public static void main(String[] args)
+    {
+        char ch = 'a';
+        // Autoboxing is Primitive to Character Object Conversion
+        Character a = ch;
+  
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
