8ed264a34000492f84d79c15bd92236b93d2c954
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-03 22:23:27 +0530

    Update tern.java

--- tern.java
+++ tern.java
@@ -9,3 +9,9 @@ public class Main {
  
         System.out.println("First num: " + n1);
         System.out.println("Second num: " + n2);
+     
+        max = (n1 > n2) ? n1 : n2;
+
+        System.out.println("Maximum is = " + max);
+    }
+}
