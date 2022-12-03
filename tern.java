41fce831cf45b2f44db402ec57f9b6775bce607a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-03 22:23:10 +0530

    Update tern.java

--- tern.java
+++ tern.java
@@ -5,3 +5,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args)
     {
+        int n1 = 5, n2 = 10, max;
+ 
+        System.out.println("First num: " + n1);
+        System.out.println("Second num: " + n2);
