0310ad2501414808a8f2bbfdb803cfa08cd0bc6b
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-05 22:51:03 +0530

    Update bit.java

--- bit.java
+++ bit.java
@@ -5,3 +5,11 @@ public class Main {
     {
         int a = 5;
         int b = 7;
+        
+        // bitwise and
+        // 0101 & 0111=0101 = 5
+        System.out.println("a&b = " + (a & b));
+ 
+        // bitwise or
+        // 0101 | 0111=0111 = 7
+        System.out.println("a|b = " + (a | b));
