a3a9e523dd3f05ec573dc4c0d7c5174b7c93ee13
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-14 23:03:27 +0530

    Update keyword9.java

--- keyword9.java
+++ keyword9.java
@@ -2,20 +2,22 @@ class Test
 {
     int a;
     int b;
- 
-    //Default constructor
     
-    Test()
+    Test()                  //Default constructor
     { 
         this(10, 20);
         System.out.println("Inside  default constructor");
     }
-    
-    //Parameterized constructor
-    
-    Test(int a, int b)
+      
+    Test(int a, int b)     //Parameterized constructor
     {
         this.a = a;
         this.b = b;
         System.out.println("Inside parameterized constructor");
     }
+    
+    public static void main(String[] args)
+    {
+        Test object = new Test();
+    }
+}
