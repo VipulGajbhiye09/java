3bc7d5fc409f858ffee793666a3f02106feb8983
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-16 22:54:38 +0530

    Update keyword11.java

--- keyword11.java
+++ keyword11.java
@@ -11,3 +11,9 @@ public class Test {
     {
         System.out.println("Inside show function");
     }
+ 
+    public static void main(String args[]) {
+        Test t1 = new Test();
+        t1.display();
+    }
+}
