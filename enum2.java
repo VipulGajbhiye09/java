de975b828a06d3f32cc20805a6cd06214b6ccd6d
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-19 23:21:20 +0530

    Update enum2.java

--- enum2.java
+++ enum2.java
@@ -8,3 +8,11 @@ public class Test {
     public static void main(String[] args)
     {
         Color arr[] = Color.values();
+        
+        for (Color col : arr) {
+            // Calling ordinal() to find index of color.
+            System.out.println(col + " at index " + col.ordinal());
+        }
+        System.out.println(Color.valueOf("RED"));
+    }
+}
