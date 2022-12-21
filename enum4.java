7295aa4022c6b5acda3784f053ad3d94b593ce9a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-21 22:06:28 +0530

    Update enum4.java

--- enum4.java
+++ enum4.java
@@ -9,3 +9,27 @@ enum Day {
     FRIDAY,
     SATURDAY;
 }
+
+public class Test {
+    Day day;
+ 
+    public Test(Day day) { this.day = day; }
+ 
+    public void dayIsLike()
+    {
+        switch (day) {
+        case MONDAY:
+            System.out.println("Mondays are bad.");
+            break;
+        case FRIDAY:
+            System.out.println("Fridays are better.");
+            break;
+        case SATURDAY:
+        case SUNDAY:
+            System.out.println("Weekends are best.");
+            break;
+        default:
+            System.out.println("Midweek days are so-so.");
+            break;
+        }
+    }
