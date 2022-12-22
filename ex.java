22305467e8d8c146077351707ce9d37d93564a93
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-22 23:16:24 +0530

    Update ex.java

--- ex.java
+++ ex.java
@@ -1,1 +1,14 @@
 
+class Student
+{ 
+    int rollno;  
+    String name;
+ 
+    public static void main(String args[])
+    { 
+        Student s1=new Student();
+      
+        System.out.println(s1.rollno); 
+        System.out.println(s1.name); 
+     } 
+} 
