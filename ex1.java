1bcfa377f985f47ed0ca7ed3a4097b7f8263e8a6
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-23 22:38:08 +0530

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -1,3 +1,13 @@
 public class Test {
     static String name;
     static float cost;
+    
+   static void set(String n, float c) {
+        name = n;
+        cost = c;
+    }
+ 
+    static void disp() {
+        System.out.println("name is: " + name);
+        System.out.println("cost is: " + cost);
+    }
