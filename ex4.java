01321f680692f37a12c83d4f0c1b5b19e0f03f1a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-27 23:03:26 +0530

    Update ex4.java

--- ex4.java
+++ ex4.java
@@ -8,4 +8,11 @@ class Test {
         ArrayList<Integer> arrli = new ArrayList<Integer>(n);
          for (int i = 1; i <= n; i++)
             arrli.add(i)
+             
        System.out.println(arrli);
+        
+        
+        for (int i = 0; i < arrli.size(); i++)
+            System.out.print(arrli.get(i) + " ");
+    }
+}
