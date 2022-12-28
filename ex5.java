8f2683667367b8c999d2e1642596b26e8a7721ab
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-28 22:21:49 +0530

    Update ex5.java

--- ex5.java
+++ ex5.java
@@ -7,3 +7,11 @@ class Employee {
 class Manager extends Employee {
     int benefits = 10000;
 }
+
+public class Main {
+    public static void main(String args[])
+    {
+        Manager obj = new Manager();
+        System.out.println("Salary : " + obj.salary + "\nBenefits : " + obj.benefits);
+    }
+}
