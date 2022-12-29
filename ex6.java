dd35d0c9669e6c5c8f43c1d17a2441c69e3a9cd7
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-29 23:47:21 +0530

    Update ex6.java

--- ex6.java
+++ ex6.java
@@ -11,3 +11,8 @@ public class Moon extends Earth {
         SolarSystem s = new SolarSystem();
         Earth e = new Earth();
         Mars m = new Mars();
+ 
+        System.out.println(s instanceof SolarSystem);
+        System.out.println(e instanceof Earth);
+        System.out.println(m instanceof SolarSystem);
+    }
