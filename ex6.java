be158eaa2a859b6839719504195a90d426e423cf
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-12-29 23:47:10 +0530

    Update ex6.java

--- ex6.java
+++ ex6.java
@@ -4,3 +4,10 @@ class Earth extends SolarSystem {
 }
 class Mars extends SolarSystem {
 }
+
+public class Moon extends Earth {
+    public static void main(String args[])
+    {
+        SolarSystem s = new SolarSystem();
+        Earth e = new Earth();
+        Mars m = new Mars();
