3ecbceca7f4d8fd174d6246df20b894bfdd14e4a
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-11 23:56:20 +0530

    Update jdbc02.java

--- jdbc02.java
+++ jdbc02.java
@@ -8,3 +8,18 @@ public class SQLPreparedStatementSelect {
     try{
     Class.forName("com.mysql.jdbc.Driver");
     Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db","root","root");
+    PreparedStatement stmt = con.prepareStatement("select * from userinfo");
+    ResultSet rs = stmt.executeQuery();
+    while(rs.next())
+    {
+    System.out.println(rs.getString(1) + " " +
+    rs.getString(2) + " " + rs.getString(3) + " " +
+    rs.getString(4) + " ");
+    }
+    con.close();
+    }
+    catch(Exception e){
+    System.out.println(e);
+    }
+   }
+  }
