0b2603e7b24f0ae44d5060893fd40e26aaf5d536
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-11 23:54:18 +0530

    Update jdbc01.java

--- jdbc01.java
+++ jdbc01.java
@@ -4,10 +4,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 public class SQLPreparedStatementInsert {
-public static void main(String[] args) {
-try{
-Class.forName("com.mysql.jdbc.Driver");
-Connection con=DriverManager.getConnection(
-"jdbc:mysql://localhost:3306/db,"root","root");
-PreparedStatement stmt = con.prepareStatement("insert into userinfo values (?,?,?,?,?)");
-stmt.setString(1,"newuser");
+  public static void main(String[] args) {
+    try{
+      Class.forName("com.mysql.jdbc.Driver");
+      Connection con=DriverManager.getConnection(
+      "jdbc:mysql://localhost:3306/db,"root","root");
+      PreparedStatement stmt = con.prepareStatement("insert into userinfo values (?,?,?,?,?)");
+      stmt.setString(1,"newuser");
+      stmt.setString(2,"pass");
+      stmt.setString(3,"user4gmail.com");
+      stmt.setString(4, "City");
+      stmt.setString(5, "25");
+      int i = stmt.executeUpdate();
+      System.out.println(i + "Records inserted..");
+      con.close();
+    }
+    catch(Exception e){
+    System.out.println(e);
+    }
+  }
+}
