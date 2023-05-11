7ac92722d7b69bfea7d2d967ea045d5625e55ee4
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-11 23:52:30 +0530

    Create jdbc01.java

---
+++ jdbc01.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+public class SQLPreparedStatementInsert {
+public static void main(String[] args) {
+try{
+Class.forName("com.mysql.jdbc.Driver");
+Connection con=DriverManager.getConnection(
+"jdbc:mysql://localhost:3306/db,"root","root");
+PreparedStatement stmt = con.prepareStatement("insert into userinfo values (?,?,?,?,?)");
+stmt.setString(1,"newuser");
