43a1b4d27b58c0e1fc618b7f681f51e28c8d3853
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-12 23:53:38 +0530

    Update jdbc03.java

--- jdbc03.java
+++ jdbc03.java
@@ -2,5 +2,15 @@ import java.sql.Connection; import java.sql.DriverManager;
 import java.sql.PreparedStatement; import java.sql.ResultSet;
 import java.sql.Statement;
 public class SQLPreparedStatementUpdate { public static void main(String[] args) {
-try{ Class.forName("com.mysql.jdbc.Driver");
-Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/db","root","root");
+  try{ Class.forName("com.mysql.jdbc.Driver");
+    Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/db","root","root");
+    PreparedStatement stmt = con.prepareStatement("update userinfo set city=? where username=?"); stmt.setString(1,"Chennai");
+    stmt.setString(2,"user2"); int i = stmt.executeUpdate();
+
+    System.out.println(i + "Records updated"); con.close();
+  }
+  catch(Exception e){ 
+    System.out.println(e);
+  }
+  }
+}
