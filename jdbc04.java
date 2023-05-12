862c8bfdfb2f1bcf6fdfe269b9cab32ed4c8d776
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-12 23:54:18 +0530

    Create jdbc04.java

---
+++ jdbc04.java
@@ -0,0 +1,7 @@
+import java.sql.Connection; import java.sql.DriverManager;
+import java.sql.PreparedStatement; import java.sql.ResultSet;
+import java.sql.Statement;
+public class SQLPreparedStatementDelete { public static void main(String[] args) {
+try{
+Class.forName("com.mysql.jdbc.Driver"); Connection con=DriverManager.getConnection(
+"jdbc:mysql://localhost:3306/db","root","root");
