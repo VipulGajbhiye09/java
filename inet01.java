41ea49d06a76bf04150f34c3adddfdf9589c3057
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-13 23:07:54 +0530

    Create inet01.java

---
+++ inet01.java
@@ -0,0 +1,9 @@
+package AJP;
+import java.io.*;
+import java.net.*;
+public class InetDemo {
+
+  public static void main(String[] args) {
+    try{
+    InetAddress ip=InetAddress.getByName("engg.dypvp.edu.in");
+
