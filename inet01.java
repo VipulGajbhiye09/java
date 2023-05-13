afea562b7595ad7fb2aa20ce211391a2cee8eca7
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-13 23:08:38 +0530

    Update inet01.java

--- inet01.java
+++ inet01.java
@@ -6,4 +6,12 @@ public class InetDemo {
   public static void main(String[] args) {
     try{
     InetAddress ip=InetAddress.getByName("engg.dypvp.edu.in");
+    System.out.println("Host Name: "+ip.getHostName()); System.out.println("IP Address:"+ip.getHostAddress());
+    }
+    catch(Exception e)
+    {
+    System.out.println(e);
+    }
+   }
+  }
 
