5c1db7fc2bfc2d8ebc1408e9f4287ca383060ec8
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-14 23:42:07 +0530

    Update servlet01.java

--- servlet01.java
+++ servlet01.java
@@ -5,6 +5,26 @@ import java.servlet.http.*;
 public class CalculatorServlet extends HttpServlet {
 public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException { int result = 0;
 try {
-String number1 = req.getParameter("num1");
-String number2 = req.getParameter("num2");
-String operator = req.getParameter("op");
+  String number1 = req.getParameter("num1");
+  String number2 = req.getParameter("num2");
+  String operator = req.getParameter("op");
+  int x = Integer.parseInt(number1);
+  int y = Integer.parseInt(number2);
+  if(operator == "+") {
+    result = x + y;
+  } 
+  else if(operator == "-") { 
+    result = x - y;
+  }
+  else if(operator == "*") {
+    result = x * y;
+  }
+  else if(operator == "/") {
+    result = x/y;
+  }
+  PrintWriter p = res.getWriter();
+  p.println(result);
+ }
+catch(Exception e) {
+ }
+}
