6169872d0d65f7358d2018f644e0fcd8522c6eb7
Vipul Kishor Gajbhiye <111264723+VipulGajbhiye09@users.noreply.github.com>
2023-05-19 23:06:55 +0530

    Create servlet02.java

---
+++ servlet02.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+
+public class Validation extends GenericServlet{
+  public void service(ServletRequest req,ServletResponse res)throws ServletException,IOException
+  {
+  PrintWriter pw=res.getWriter(); String 
